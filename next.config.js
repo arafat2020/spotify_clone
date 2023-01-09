e9e1627633e0579d35fb8e66971e26fdaf3f3edc
Arafat <arafatmannan2019@gmail.com>
2023-01-09 21:31:44 +0600

    env fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    NEXT_CLIENT_SECRET:process.env.NEXT_CLIENT_SECRET,
-    NEXT_CLIENT_ID:process.env.NEXT_CLIENT_ID,
-    NEXT_JWT_SECRET:process.env.NEXT_JWT_SECRET,
-    NEXTAUTH_URL:process.env.NEXTAUTH_URL
-  }
+  
 }
 
 module.exports = nextConfig
