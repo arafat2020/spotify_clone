2246b66e6f8ed3e0f049f7e06664ebdb6eb59382
Arafat <arafatmannan2019@gmail.com>
2023-01-09 23:22:32 +0600

    bal ar bal

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,15 @@
 import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-function Login({ provider }) {
-  const router = useRouter()
-  const {status} = useSession()
+export default function Login() {
+  const [pr,setPr]=useState()
+  const provider = async () => await getProviders();
+  provider().then(res=>{
+    setPr(res.spotify.id);
+  })
+  const router = useRouter();
+  const { status } = useSession();
   useEffect(() => {
     if (status === "authenticated") {
       const reDirect = () => router.push("/");
@@ -14,30 +19,21 @@ function Login({ provider }) {
   return (
     <div className="w-screen h-screen bg-black flex flex-col justify-center">
       <div className="flex flex-col items-center">
-        <img className="w-[300px] h-[120px]" src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg"/>
-        {Object.values(provider).map((e) => {
-          return (
-            <button
-             className="bg-green-500 text-white font-semibold p-3 rounded-md"
-              key={e.id}
-              onClick={() => signIn(e.id, { callbackUrl: "/" })}
-            >
-              Login with {e.name}
-            </button>
-          );
-        })}
+        <img
+          className="w-[300px] h-[120px]"
+          src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg"
+        />
+        {pr && (
+          <button
+            className="bg-green-500 text-white font-semibold p-3 rounded-md"
+            key={pr}
+            onClick={() => signIn(pr, { callbackUrl: "/" })}
+          >
+            Login with {pr}
+          </button>
+        )}
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const provider = await getProviders();
-  return {
-    props: {
-      provider,
-    },
-  };
-}
-
-export default Login;
